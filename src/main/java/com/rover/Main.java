9565bbb790f4e26625e067499677f3893ff2ed64
Omar Guermazi <guerm@Omar>
2026-06-23 17:06:41 +0200

    chore: delete IntelliJ comment.

--- src/main/java/com/rover/Main.java
+++ src/main/java/com/rover/Main.java
@@ -11,8 +11,6 @@ import com.rover.parser.TextInputParserImpl;
 
 import java.util.List;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
     public static void main(String[] args) throws Exception {
